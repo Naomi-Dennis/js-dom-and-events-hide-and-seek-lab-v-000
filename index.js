0e6f0e2fc24de07c7cf6b2b1e58f25407c51e083
Naomi Dennis <njdennis94@gmail.com>
2018-10-02 19:40:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,58 @@
+  // describe('getFirstSelector(selector)', () => {
+  //   it('returns the first element that matches the selector', () => {
+  //     expect(getFirstSelector('div').id).toEqual('nested')
+  //     expect(getFirstSelector('.ranked-list')).toEqual(document.querySelector('.ranked-list'))
+  //   })
+  // })
+  
+  function getFirstSelector(selector){
+    return document.querySelector(selector) 
+  }
+  
+  // describe('nestedTarget()', () => {
+  //   it('pulls a .target out of #nested', () => {
+  //     expect(nestedTarget()).toEqual(document.querySelector('#nested .target'))
+  //   })
+  // })
+  function nestedTarget(){
+    return document.querySelector('#nested .target')
+  }
+  
+  
+  // describe('deepestChild()', () => {
+  //   it('returns the most deeply nested child in #grand-node', () => {
+  //     console.log(deepestChild().innerHTML)
+  //     expect(deepestChild()).toBe(document.querySelector('#grand-node div div div div'))
+  //   })
+  // })
+  
+  function deepestChild(){
+    let root = document.querySelector("#grand-node")
+    let deepNodes = findDeepestChild(root, 0 )
+    let lowestNode = deepNodes.reduce( (node, iter) => node = (iter.level < node.level) ? iter : node  )
+    
+    return lowestNode.value
+  }
+  
+  function findDeepestChild(root, level){
+    let childNodes = Array.from(root.children)
+    if(childNodes.length == 0){
+      return {value: root, level: level}
+    } 
+    
+    return childNodes.reduce( (accumulator, itr)=>
+    { 
+      accumulator = accumulator.concat( findDeepestChild(itr, level + 1));                        
+      return accumulator; 
+      
+    }, [] ) 
+  }
+
+  function increaseRankBy(n){
+    let nodes = Array.from( document.querySelectorAll(".ranked-list") )
+    nodes.map( (par_itr) => { 
+      Array.from(par_itr.children).map( (child_itr) =>{
+        child_itr.innerHTML = parseInt(child_itr.innerHTML) + n
+      })
+    })
+  }
