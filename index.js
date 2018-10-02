93fc5af8e5d0f510ec61de70df5bd2944f5195e4
Naomi Dennis <njdennis94@gmail.com>
2018-10-02 19:09:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -39,7 +39,7 @@
       return {value: root, level: level}
     } 
     
-    return [].concat( childNodes.map( (itr)=>{ return findDeepestChild(itr, level + 1) } ) )
+    return ( childNodes.map( (itr)=>{ return findDeepestChild(itr, level + 1) } ) )
     
   }
 
